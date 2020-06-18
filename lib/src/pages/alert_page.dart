10a651146a8a3b30f38a60a9bb011ec12d5b9156
Celia Navarro Pérez <celiinavarroperez@gmail.com>
2020-06-18 10:23:25 +0100

    AlertPage completed with its alert box

--- lib/src/pages/alert_page.dart
+++ lib/src/pages/alert_page.dart
@@ -9,6 +9,16 @@ class AlertPage extends StatelessWidget {
    appBar: AppBar(
      title: Text('Alert Page'),
    ),
+   body: Center(
+     child: RaisedButton(
+       child: Text('Mostrar Alerta'),
+       color: Colors.blue,
+       textColor: Colors.white,
+       shape: StadiumBorder(),
+       onPressed:()=> _showAlert(context),
+
+     ),
+   ),
    floatingActionButton: FloatingActionButton(
      child: Icon(Icons.arrow_back_ios),
      onPressed: (){
@@ -17,4 +27,40 @@ class AlertPage extends StatelessWidget {
    ),
  );
   }
+
+void _showAlert(BuildContext context){
+
+  showDialog(
+    context: context,
+    barrierDismissible: true,
+    builder: (context) {
+
+      return AlertDialog(
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20) ),
+        title: Text('Título'),
+        content: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: <Widget>[
+            Text('Este es el contenido de la alerta'),
+            FlutterLogo(size:100)
+          ],
+        ),
+        actions: <Widget>[
+          FlatButton(
+            child: Text('Cancelar'),
+            onPressed: () => Navigator.of(context).pop(),
+          ),
+          FlatButton(
+            child: Text('OK'),
+            onPressed: ()=> Navigator.of(context).pop()
+
+          )
+        ],
+      );
+    },
+    
+    );
+
+}
+
 }
