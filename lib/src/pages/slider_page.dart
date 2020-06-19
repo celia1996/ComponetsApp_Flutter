ef83e628ecca5e9219440612f208e8c62dea2468
Celia Navarro Pérez <celiinavarroperez@gmail.com>
2020-06-19 16:28:21 +0100

    Added checkbox and switch to lock the slider

--- lib/src/pages/slider_page.dart
+++ lib/src/pages/slider_page.dart
@@ -9,6 +9,7 @@ class SliderPage extends StatefulWidget {
 
 class _SliderPageState extends State<SliderPage> {
   double _sliderValue = 100.0;
+  bool _lockImage = true;
 
   @override
   Widget build(BuildContext context) {
@@ -21,10 +22,10 @@ class _SliderPageState extends State<SliderPage> {
         child: Column(
           children: <Widget>[
             _createSlider(),
-            Expanded(
-              child: _createImage()
-              )
-             ],
+            _createCheckBox(),
+            _createSwitch(),
+            Expanded(child: _createImage())
+          ],
         ),
       ),
     );
@@ -38,19 +39,55 @@ class _SliderPageState extends State<SliderPage> {
       value: _sliderValue,
       min: 10.0,
       max: 400.0,
+      onChanged: (_lockImage)
+          ? null
+          : (value) {
+              setState(() {
+                _sliderValue = value;
+              });
+            },
+    );
+  }
+
+  Widget _createCheckBox() {
+    // return Checkbox(
+    //   value: _lockImage,
+    //   onChanged: (value){
+    //     setState(() {
+    //     _lockImage = value;
+    //     });
+    //   },
+    //   );
+
+    return CheckboxListTile(
+      title: Text('Bloquear slider'),
+      value: _lockImage,
       onChanged: (value) {
         setState(() {
-          _sliderValue = value;
+          _lockImage = value;
         });
       },
     );
   }
 
- Widget  _createImage() {
-   return Image(
-     image: NetworkImage('https://p4.wallpaperbetter.com/wallpaper/441/29/100/red-mushroom-between-two-large-and-small-mushrooms-on-green-grasses-near-tree-wallpaper-preview.jpg'),
-     width: _sliderValue,
-     fit: BoxFit.contain,
-     );
- }
+  Widget _createImage() {
+    return Image(
+      image: NetworkImage(
+          'https://p4.wallpaperbetter.com/wallpaper/441/29/100/red-mushroom-between-two-large-and-small-mushrooms-on-green-grasses-near-tree-wallpaper-preview.jpg'),
+      width: _sliderValue,
+      fit: BoxFit.contain,
+    );
+  }
+
+  Widget _createSwitch() {
+    return SwitchListTile(
+      title: Text('Bloquear slider'),
+      value: _lockImage,
+      onChanged: (value) {
+        setState(() {
+          _lockImage = value;
+        });
+      },
+    );
+  }
 }
